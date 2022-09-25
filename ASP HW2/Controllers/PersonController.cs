673d935da5ffa14dcf569625b095947a1f3f5651
Mətin Abaszadə <metin.abaszade@gmail.com>
2022-09-25 21:02:19 +0400

    Add project files.

---
+++ ASP HW2/Controllers/PersonController.cs
@@ -0,0 +1,52 @@
+﻿using ASP_HW2.Entitities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace ASP_HW2.Controllers
+{
+    public class PersonController : Controller
+    {
+
+        public static List<Person> People = new List<Person>()
+        {
+            new Person("Polad ", "Bulbuloğlu", 77, "uploads/Polad Bulbuloqlu.jpg", null),
+            new Person("Mübariz", "Tağıyev", 74, "uploads/Mubariz Tagiiyev.jpg", null),
+            new Person("Ağadadaş", "Ağayev", 66, "uploads/Aqadadas Aqayev.jpg", null),
+            new Person("Flora", "Karimova", 81, "uploads/Flora Kerimova.jpg", null)
+        };
+
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View(People);
+        }
+
+        // POST: Person/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Person newperson)
+        {
+            string fileextension = Path.GetExtension(newperson.MyFile.FileName);
+            string filename = Path.ChangeExtension(
+                    Path.GetRandomFileName(),
+                    fileextension
+            );
+            string filelocation = "wwwroot/uploads/" + filename;
+            newperson.MyFile.CopyTo(new FileStream(filelocation, FileMode.Create));
+            newperson.Id = ++Person.LastId;
+            newperson.Image = "uploads/" + filename;
+            newperson.MyFile = null;
+            People.Add(newperson);
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ ASP HW2/Entitities/Person.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ASP_HW2.Entitities
+{
+    public class Person
+    {
+        public static int LastId { get; set; } = 0;
+
+        public Person()
+        {
+
+        }
+
+        public Person(string name, string surname, int age, string ımage, IFormFile file)
+        {
+            Id = ++LastId;
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Image = ımage;
+            MyFile = file;
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Image { get; set; }
+
+        public IFormFile  MyFile { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Id: {this.Id}" +
+                   $"Name: {this.Name}" +
+                   $"Surname: {this.Surname}" +
+                   $"Age: {this.Age}" +
+                   $"Image: {this.Image}";
+        }
+    }
+}
